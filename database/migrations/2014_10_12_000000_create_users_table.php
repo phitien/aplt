00b24bab6876eaaaac5b0d00e0cada2a2dbb79dd
Phi Tien <ductien.phi@bosch-si.com>
2016-04-11 11:26:10 +0800

    Initiation with jwt-auth

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,51 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreateUsersTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create ( 'users', function (Blueprint $table) {
+			$table->increments ( 'id' );
+			$table->string ( 'name' )->unique ();
+			$table->string ( 'email' )->unique ();
+			$table->string ( 'password' );
+			$table->boolean ( 'active' )->nullable ();
+			$table->string ( 'second_email' )->nullable ();
+			$table->string ( 'first_name' )->nullable ();
+			$table->string ( 'last_name' )->nullable ();
+			$table->string ( 'middle_name' )->nullable ();
+			$table->string ( 'alias' )->nullable ();
+			$table->date ( 'birthday' )->nullable ();
+			$table->enum ( 'gender', array (
+					'FEMALE',
+					'MALE' 
+			) )->default ( 'MALE' )->nullable ();
+			$table->string ( 'mobile' )->unique ( 'mobile', 'user_mobile_unique' )->nullable ();
+			$table->string ( 'country' )->nullable ();
+			$table->string ( 'state' )->nullable ();
+			$table->string ( 'city' )->nullable ();
+			$table->string ( 'address' )->nullable ();
+			$table->string ( 'avatar' )->nullable ();
+			$table->string ( 'cover' )->nullable ();
+			$table->text ( 'status' )->nullable ();
+			$table->text ( 'quote' )->nullable ();
+			$table->text ( 'description' )->nullable ();
+			$table->text ( 'json' )->nullable ();
+			$table->rememberToken ();
+			$table->timestamps ();
+		} );
+	}
+	
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists ( 'users' );
+	}
+}
---
+++ database/migrations/2016_04_07_093710_create_user_role_table.php
@@ -0,0 +1,34 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class CreateUserRoleTable extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create ( 'user_role', function (Blueprint $table) {
+			$table->increments ( 'id' );
+			$table->integer ( 'user_id' )->unsigned ();
+			$table->foreign ( 'user_id' )->references ( 'id' )->on ( 'users' )->onDelete ( 'cascade' );
+			$table->integer ( 'role_id' )->unsigned ();
+			$table->foreign ( 'role_id' )->references ( 'id' )->on ( 'roles' )->onDelete ( 'cascade' );
+			$table->boolean ( 'active' );
+			$table->unique ( [ 
+					'user_id',
+					'role_id' 
+			], 'user_role_unique' );
+		} );
+	}
+	
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::dropIfExists ( 'user_role' );
+	}
+}
+	
